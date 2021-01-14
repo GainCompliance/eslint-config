a433564a4d261aea332b101a3040e40afdbfcab4
Travis Peterson <contact@travispeterson.us>
2021-01-14 10:41:15 -0600

    fix: removes form8ion eslint config dependency

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 module.exports = {
-  extends: '@form8ion'
+  plugins: ['filenames'],
+
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:import/recommended',
+    'plugin:eslint-comments/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  rules: {
+    'no-console': 'error',
+    'comma-dangle': ['error', 'never'],
+    'object-curly-spacing': ['error', 'never'],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline: true,
+          consistent: true
+        },
+        ObjectPattern: {
+          multiline: true,
+          consistent: true
+        },
+        ImportDeclaration: {
+          multiline: true,
+          consistent: true,
+          minProperties: 0
+        }
+      }
+    ],
+    'filenames/match-regex': ['error', '^[a-z0-9\\-\\.]+$'],
+    'arrow-parens': ['error', 'as-needed'],
+    yoda: ['error', 'always'],
+    'no-multi-spaces': ['error', {ignoreEOLComments: true}],
+    'max-len': ['error', 120],
+    'one-var': ['error', {uninitialized: 'always', initialized: 'never'}],
+    'one-var-declaration-per-line': ['error', 'initializations'],
+    'global-require': 'off',
+    'import/prefer-default-export': 'off'
+  }
 };
